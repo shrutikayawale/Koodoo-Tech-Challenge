08e886f41bca1c561654d1f81ff293ed1cc6b372
Shrutika Yawale <42666782+shrutikayawale@users.noreply.github.com>
2021-12-07 18:37:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,9 @@ const accountBalanceHistory = [
     Case 1 - Type A denotes a balance history where the balance amount decreases by varying amounts each month.
     Case 2 - Type B is one where the balance amount changes by the same amount each month.
              (This inlcudes both incrases and decreases by same amount)
-    Case 3 - Type C denotes a balance history where the balance amount increases by varying amounts each month.
+    Case 3 - Type C denotes a balance history where the balance amount increases by varying amounts each month.  
+    
+    Note: I am assuming type 'C' for above case
 ***/
 
 const accountTypeChecker = (accountBalanceHistory) => {
@@ -36,12 +38,15 @@ const accountTypeChecker = (accountBalanceHistory) => {
             diff = accountBalanceHistory[i + 1].account.balance.amount - accountBalanceHistory[i].account.balance.amount;
         } else if ((i + 1) < accountBalanceHistory.length && accountBalanceHistory[i + 1] && accountBalanceHistory[i + 1].account.balance.amount - accountBalanceHistory[i].account.balance.amount == diff) {
             //for every elements we have to find out the diff of amount as compared to the next element except last (bcz there wont be any element after last element)
+            console.log('Your account type is B (amount will be either increasing or decreasing)');
             result = 'B';
         } else if (accountBalanceHistory[i + 1].account.balance.amount > accountBalanceHistory[i].account.balance.amount) {
             //the balance amount decreases by varying amounts each month
+            console.log('Your account type is A (amount will be decreasing by variable amount)');
             result = 'A';
         } else {
             //the balance amount increases by varying amounts each month
+            console.log('Your account type is C (amount will be increasing by variable amount)');
             result = 'C'; 
         }
     }
